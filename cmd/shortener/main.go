4dd8e5d9c326ccdccee428d265852651cdf0c33e
Denys Borysenko <Deni.Borisenko@x5.ru>
2026-02-01 23:11:22 +0300

    add handlers, setup server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"github.com/DenysBorysenko1/shorturl/internal/handler"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handler.Generate)
+	mux.HandleFunc(`/{id}/`, handler.Retrieve)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
