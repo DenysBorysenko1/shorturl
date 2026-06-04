bc378bf6af6a5d8b43a228adf7ba8d03a4c21990
Denys Borysenko <Deni.Borisenko@x5.ru>
2026-06-04 21:33:59 +0300

    [iter23] add vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"shorturl/internal/audit"
 	"shorturl/internal/config"
@@ -20,7 +21,17 @@ import (
 	_ "github.com/jackc/pgx/v5"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	config.Load()
 	logger.Initialize(config.Cfg.LogLevel)
 
